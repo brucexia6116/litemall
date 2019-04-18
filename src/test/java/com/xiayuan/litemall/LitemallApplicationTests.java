da701edbed967198242f063e4a53008d2e7831f3
brucexia6116 <bruce_xia6116@163.com>
2019-04-18 10:40:13 +0800

    Initial commit

---
+++ src/test/java/com/xiayuan/litemall/LitemallApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiayuan.litemall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LitemallApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
